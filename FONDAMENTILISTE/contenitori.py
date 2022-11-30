3fc625e914110d0c1c0cff86a9a294248047d9cb
Alessandro1805 <113350229+Alessandro1805@users.noreply.github.com>
2022-11-30 17:39:16 +0100

    Add files via upload

---
+++ FONDAMENTILISTE/contenitori.py
@@ -0,0 +1,8 @@
+#contenitori liste tuple dizionari permettono di salvare più varibili in una struttura dati
+#liste
+#slycing come nelle liste come anche la stampa indicizzazione mutabili
+#posso aggiungere elementi
+l=[9,8,7,7,8,6]
+print(l[1:-1])
+l[4]=10
+print(l[0:3])
---
+++ FONDAMENTILISTE/contenitori2.py
@@ -0,0 +1,8 @@
+x=[1,2,3,4,5,6,7,8]
+a=len(x)//2
+l1=x[:a]
+l2=x[a:]
+print(l1)
+print(l2)
+l1.append(5)
+print(l1)
